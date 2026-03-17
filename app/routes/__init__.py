635aaeb1372b20845aaeec7c82350e8cb6b0ee39
LauncherEdtech <155194223+LauncherEdtech@users.noreply.github.com>
2026-03-16 21:03:59 -0300

    Delete app directory

--- app/routes/__init__.py
+++
@@ -1,7 +0,0 @@
-from app.routes.health import health_bp
-from app.routes.users import users_bp
-
-
-def register_blueprints(app):
-    app.register_blueprint(health_bp)
-    app.register_blueprint(users_bp)
